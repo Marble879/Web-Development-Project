d6ce29bff7a76d144629df35830818bc4cebe5b2
younisa <younisa@student.chalmers.se>
2021-09-08 18:01:19 +0200

    Revert "#1 [GROUP PROGRAMMING] Add user entity model"
    This reverts commit 8763502039ff4d6c5dd9bdfe3a91b9b7daf6cb1f

--- server/models/user.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-
-var userSchema = new Schema({
-  username: {  type: String, unique: true, required: true},
-  password: {  type: String, required: true},
-  )
-  bio : { type : String}
