8763502039ff4d6c5dd9bdfe3a91b9b7daf6cb1f
younisa <younisa@student.chalmers.se>
2021-09-08 17:48:14 +0200

    #1 [GROUP PROGRAMMING] Add user entity model
    Co-authored-by: nilsdu <nilsdu@student.chalmers.se>
    Co-authored-by: sicily <sicily@net.chalmers.se>

---
+++ server/models/user.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  username: {  type: String, unique: true, required: true},
+  password: {  type: String, required: true},
+  )
+  bio : { type : String}
