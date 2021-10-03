3129f4e9a0f9b2a79e86986ef0eb1dc46d9e0d8a
younisa <younisa@student.chalmers.se>
2021-10-03 23:22:05 +0200

    #13 #15 Add structure and responsiveness when displaying post images in a collection

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,7 +6,7 @@
       <router-link to="/login">Login</router-link> |
       <router-link to="/posts/create">Create a post</router-link> |
       <!-- Temporary way to get into the collections view page until the user page is complete -->
-      <router-link to="/user/:Uid/collection/:Cid">Inside a collection</router-link>
+      <router-link to="/users/:Uid/collection/:Cid">Inside a collection</router-link>
     </div>
     <!-- Render the content of the current page view -->
     <router-view />
--- client/src/views/InsideCollection.vue
+++ client/src/views/InsideCollection.vue
@@ -1,13 +1,68 @@
 <template>
     <b-container>
-
+        <b-row>
+          <b-col v-for="post in posts" v-bind:key="post._id" lg="4">
+            <b-img center thumbnail fluid block rounded v-bind:src="post.image"/>
+          </b-col>
+        </b-row>
     </b-container>
 </template>
 
 <script>
 
 export default {
-  name: 'InsideCollection'
+  name: 'InsideCollection',
+  data() {
+    return {
+      posts: [
+        {
+          _id: '613885d342fb9851638791b5',
+          title: 'testT1',
+          image: 'http://localhost:3000/uploads/1633292779196.jpg'
+        },
+        {
+          _id: '6138862842fb9851638791b7',
+          title: 'testT2',
+          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+        },
+        {
+          _id: '6138862842fb9851638791b7',
+          title: 'testT2',
+          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+        },
+        {
+          _id: '6138862842fb9851638791b7',
+          title: 'testT2',
+          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+        },
+        {
+          _id: '6138862842fb9851638791b7',
+          title: 'testT2',
+          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+        },
+        {
+          _id: '6138862842fb9851638791b7',
+          title: 'testT2',
+          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+        },
+        {
+          _id: '6138862842fb9851638791b7',
+          title: 'testT2',
+          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+        },
+        {
+          _id: '6138862842fb9851638791b7',
+          title: 'testT2',
+          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+        },
+        {
+          _id: '6138862842fb9851638791b7',
+          title: 'testT2',
+          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+        }
+      ]
+    }
+  }
 }
 
 </script>
