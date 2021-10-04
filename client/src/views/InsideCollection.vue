9d85e03b640cabb1e056265b766ca2674068e60b
younisa <younisa@student.chalmers.se>
2021-10-04 20:33:15 +0200

    #13 Refactor code to use bootstrapvue cards rather than just displaying the raw images

--- client/src/views/InsideCollection.vue
+++ client/src/views/InsideCollection.vue
@@ -1,8 +1,17 @@
 <template>
     <b-container>
+        <b-container>
+          <h1>Collection name</h1>
+        </b-container>
+
         <b-row>
-          <b-col v-for="post in posts" v-bind:key="post._id" lg="4">
-            <b-img center thumbnail fluid block rounded v-bind:src="post.image"/>
+          <b-col v-for="post in posts" v-bind:key="post._id" lg="4" class="mb-3">
+            <b-card v-bind:title="post.title"
+            v-bind:img-src="post.image"
+            img-alt="Image"
+            img-top>
+            <b-button href="/" variant="primary">Go to post</b-button>
+            </b-card>
           </b-col>
         </b-row>
     </b-container>
@@ -18,47 +27,47 @@ export default {
         {
           _id: '613885d342fb9851638791b5',
           title: 'testT1',
-          image: 'http://localhost:3000/uploads/1633292779196.jpg'
+          image: 'http://localhost:3000/uploads/1633296835563.jpg'
         },
         {
           _id: '6138862842fb9851638791b7',
           title: 'testT2',
-          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+          image: 'http://localhost:3000/uploads/1633296835563.jpg'
         },
         {
           _id: '6138862842fb9851638791b7',
           title: 'testT2',
-          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+          image: 'http://localhost:3000/uploads/1633296835563.jpg'
         },
         {
           _id: '6138862842fb9851638791b7',
           title: 'testT2',
-          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+          image: 'http://localhost:3000/uploads/1633296857055.jpg'
         },
         {
           _id: '6138862842fb9851638791b7',
           title: 'testT2',
-          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+          image: 'http://localhost:3000/uploads/1633296857055.jpg'
         },
         {
           _id: '6138862842fb9851638791b7',
           title: 'testT2',
-          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+          image: 'http://localhost:3000/uploads/1633296857055.jpg'
         },
         {
           _id: '6138862842fb9851638791b7',
           title: 'testT2',
-          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+          image: 'http://localhost:3000/uploads/1633296835563.jpg'
         },
         {
           _id: '6138862842fb9851638791b7',
           title: 'testT2',
-          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+          image: 'http://localhost:3000/uploads/1633296835563.jpg'
         },
         {
           _id: '6138862842fb9851638791b7',
           title: 'testT2',
-          image: 'http://localhost:3000/uploads/1633292904721.jpg'
+          image: 'http://localhost:3000/uploads/1633296835563.jpg'
         }
       ]
     }
